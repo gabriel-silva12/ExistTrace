59bed3b6db5ef7388f61d960d43ddcfa61c9c39e
gabriel-silva12 <gabrielcesar16@gmail.com>
2026-09-10 16:30:33 -0300

    navegação alterada de  slot para stack

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,7 @@
-import { Slot } from "expo-router";
+import { Stack } from "expo-router"
 
 const RootLayout = () => {
-  return <Slot></Slot>
+  return <Stack></Stack>
 }
 
 export default RootLayout
