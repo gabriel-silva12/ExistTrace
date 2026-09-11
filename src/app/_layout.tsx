565889f12888483baa2fce623a17e61048cc299a
gabriel-silva12 <gabrielcesar16@gmail.com>
2026-09-11 17:48:46 -0300

    outras configuraçoes relacionada a supabase

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,9 @@
 import { Stack } from "expo-router"
 
 const RootLayout = () => {
-  return <Stack></Stack> 
+  return <Stack screenOptions={{
+    headerShown: false
+  }}></Stack> 
 }
 
 export default RootLayout
